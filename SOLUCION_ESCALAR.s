d44637d99f385286356192c6a7361ccb26ba8dfe
Jon O'Reilly del Cerro <jonoreilly007@gmail.com>
2025-11-08 13:08:40 +0100

    escalar v3

--- SOLUCION_ESCALAR.s
+++ SOLUCION_ESCALAR.s
@@ -16,79 +16,84 @@
         ; r0: valor 0
 
         ; r1: value of N (n)
+        ; r2: size of elements (elementSize)
 
-        ; r2: row index (i)
-        ; r3: column index (j)
+        ; r3: row index (i)
+        ; r4: column index (j)
 
-        ; r4: temp result cell value (sum)
-        ; r5: temp result cell value (mult)
+        ; r5: temp result cell value (sum)
+        ; r6: temp result cell value (mult)
         
-        ; r6: temp value MATRIZ_A[i][j] (elementA)
-        ; r7: temp value VECTOR_X[j] (elementX)
+        ; r7: temp value MATRIZ_A[i][j] (elementA)
+        ; r8: temp value VECTOR_X[j] (elementX)
 
-        ; r8: offset calculation A (offsetRowElementsA)
-        ; r9: offset calculation A (offsetElementsA)
-        ; r10: offset calculation A (offsetA)
-        ; r11: offset calculation A (addressElementA)
+        ; r9: offset calculation A (offsetRowElementsA)
+        ; r10: offset calculation A (offsetElementsA)
+        ; r11: offset calculation A (offsetA)
+        ; r12: offset calculation A (addressElementA)
         
-        ; r12: offset calculation X (offsetX)
-        ; r13: offset calculation X (addressElementX)
+        ; r13: offset calculation X (offsetX)
+        ; r14: offset calculation X (addressElementX)
         
-        ; r14: offset calculation B (offsetB)
-        ; r15: offset calculation B (addressElementB)
+        ; r15: offset calculation B (offsetB)
+        ; r16: offset calculation B (addressElementB)
         
-        ; r16: temp variable for jumps and such (temp)
+        ; r17: temp variable for jumps and such (temp)
 
         ; Load n from N
         lw r1, N                                    ; n = *N
 
+        ; Set element size to 8 bytes (.double)
+        addi r2, r0, 8                              ; elementSize = 8
+
+
         setup_loop_i:
-                addi r2, r0, 0                      ; i = 0
+                addi r3, r0, 0                      ; i = 0
         start_loop_i:
-                slti r16, r2, r1                    ; if i < n
-                beqz r16, end_loop_i                ; then goto end_loop_i
+                slti r17, r3, r1                    ; if i < n
+                beqz r17, end_loop_i                ; then goto end_loop_i
                 nop
         body_loop_i:
 
                 ; Initialize variables 
-                addi r4, r0, 0                      ; sum = 0
+                addi r5, r0, 0                      ; sum = 0
 
                 setup_loop_j:
-                        addi r3, r0, 0              ; j = 0
+                        addi r4, r0, 0              ; j = 0
                 start_loop_j:
-                        slti r16, r3, r1            ; if j < n
-                        beqz r16, end_loop_j        ; then goto end_loop_j
+                        slti r17, r4, r1            ; if j < n
+                        beqz r17, end_loop_j        ; then goto end_loop_j
                         nop
                 body_loop_j:
 
                         ; Load elementA from MATRIZ_A[i][j]
-                        mult r8, r2, r1             ; offsetRowElementsA = i * n
-                        add r9, r8, r2              ; offsetElementsA = offsetRowElementsA + j                              
-                        mult r10, r9, size          ; offsetA = offsetElementsA * size
-                        add r11, addressA, r10      ; addressElementA = addressA + offsetA
-                        lw r6, 0(r11)               ; elementA = *addressElementA
+                        mult r9, r3, r1             ; offsetRowElementsA = i * n
+                        add r10, r9, r3             ; offsetElementsA = offsetRowElementsA + j                              
+                        mult r11, r10, r2           ; offsetA = offsetElementsA * elementSize
+                        add r12, addressA, r11      ; addressElementA = addressA + offsetA
+                        lw r7, 0(r12)               ; elementA = *addressElementA
                         
                         ; Load elementX from VECTOR_X[j]                         
-                        mult r12, r2, size          ; offsetX = j * size
-                        add r13, addressX, r12      ; addressElementX = addressX + offsetX
-                        lw r7, 0(r13)               ; elementX = *addressElementX
+                        mult r13, r3, r2            ; offsetX = j * elementSize
+                        add r14, addressX, r13      ; addressElementX = addressX + offsetX
+                        lw r8, 0(r14)               ; elementX = *addressElementX
 
                         ; Calculate mult value
-                        mult r5, r6, r7             ; mult = elementA * elementX
+                        mult r6, r7, r8             ; mult = elementA * elementX
                         
                         ; Update sum value
-                        add r4, r4, r5              ; sum = sum + mult
+                        add r5, r5, r6              ; sum = sum + mult
 
                 tail_loop_j:
-                        addi r2, r2, 1              ; j++
+                        addi r3, r3, 1              ; j++
                         j start_loop_j              ; goto start_loop_j
                         nop
                 end_loop_j:
                 
                 // Store sum in RESULTADO_B[i]
-                mult r14, r0, size                  ; offsetB = i * size
-                add r15, addressB, r14              ; addressElementB = addressB + offsetB
-                sw 0(r13), sum                      ; *addressElementB = sum
+                mult r15, r0, r2                    ; offsetB = i * elementSize
+                add r16, addressB, r15              ; addressElementB = addressB + offsetB
+                sw 0(r14), sum                      ; *addressElementB = sum
                 
         tail_loop_i:
                 addi r0, r0, 1                      ; i++
