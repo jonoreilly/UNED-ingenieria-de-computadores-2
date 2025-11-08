f8398a937f9db09610ef38642383a7bc026795f0
Jon O'Reilly del Cerro <jonoreilly007@gmail.com>
2025-11-08 13:49:11 +0100

    hacer flotantes de precision doble

--- SOLUCION_ESCALAR.s
+++ SOLUCION_ESCALAR.s
@@ -1,4 +1,5 @@
 .data
+
         ; matriz NxN
         MATRIZ_A: .double 11,12,13,21,22,23,31,33,33
         
@@ -10,6 +11,9 @@
         
         ; M = N = 3. P = 1
         N: .word 3
+
+        ; valor 0
+        ZERO: .double 0
         
 .text
 
@@ -21,11 +25,11 @@
         ; r3: row index (i)
         ; r4: column index (j)
 
-        ; r5: temp result cell value (sum)
-        ; r6: temp result cell value (mult)
+        ; f0: temp result cell value (sum)
+        ; f2: temp result cell value (mult)
         
-        ; r7: temp value MATRIZ_A[i][j] (elementA)
-        ; r8: temp value VECTOR_X[j] (elementX)
+        ; f4: temp value MATRIZ_A[i][j] (elementA)
+        ; f6: temp value VECTOR_X[j] (elementX)
 
         ; r9: offset calculation A (offsetRowElementsA)
         ; r10: offset calculation A (offsetElementsA)
@@ -50,18 +54,18 @@
         setup_loop_i:
                 addi r3, r0, 0                          ; i = 0
         start_loop_i:
-                slti r17, r3, r1                        ; if i < n
+                slt r17, r3, r1                         ; if i < n
                 beqz r17, end_loop_i                    ; then goto end_loop_i
                 nop
         body_loop_i:
 
                 ; Initialize variables 
-                addi r5, r0, 0                          ; sum = 0
+                ld f0, ZERO                             ; sum = 0
 
                 setup_loop_j:
                         addi r4, r0, 0                  ; j = 0
                 start_loop_j:
-                        slti r17, r4, r1                ; if j < n
+                        slt r17, r4, r1                 ; if j < n
                         beqz r17, end_loop_j            ; then goto end_loop_j
                         nop
                 body_loop_j:
@@ -70,19 +74,19 @@
                         mult r9, r3, r1                 ; offsetRowElementsA = i * n
                         add r10, r9, r3                 ; offsetElementsA = offsetRowElementsA + j                              
                         mult r11, r10, r2               ; offsetA = offsetElementsA * elementSize
-                        add r12, MATRIZ_A, r11          ; addressElementA = addressA + offsetA
-                        lw r7, 0(r12)                   ; elementA = *addressElementA
+                        addi r12, r11, MATRIZ_A         ; addressElementA = addressA + offsetA
+                        ld f4, 0(r12)                   ; elementA = *addressElementA
                         
                         ; Load elementX from VECTOR_X[j]                         
                         mult r13, r3, r2                ; offsetX = j * elementSize
-                        add r14, VECTOR_X, r13          ; addressElementX = addressX + offsetX
-                        lw r8, 0(r14)                   ; elementX = *addressElementX
+                        addi r14, r13, VECTOR_X         ; addressElementX = addressX + offsetX
+                        ld f6, 0(r14)                   ; elementX = *addressElementX
 
                         ; Calculate mult value
-                        mult r6, r7, r8                 ; mult = elementA * elementX
+                        multd f2, f4, f6                ; mult = elementA * elementX
                         
                         ; Update sum value
-                        add r5, r5, r6                  ; sum = sum + mult
+                        addd f0, f0, f2                 ; sum = sum + mult
 
                 tail_loop_j:
                         addi r3, r3, 1                  ; j++
@@ -90,10 +94,10 @@
                         nop
                 end_loop_j:
                 
-                // Store sum in RESULTADO_B[i]
+                ; Store sum in RESULTADO_B[i]
                 mult r15, r0, r2                        ; offsetB = i * elementSize
-                add r16, RESULTADO_B, r15               ; addressElementB = addressB + offsetB
-                sw 0(r14), sum                          ; *addressElementB = sum
+                addi r16, r15, RESULTADO_B              ; addressElementB = addressB + offsetB
+                sd 0(r14), f0                           ; *addressElementB = sum
                 
         tail_loop_i:
                 addi r0, r0, 1                          ; i++
