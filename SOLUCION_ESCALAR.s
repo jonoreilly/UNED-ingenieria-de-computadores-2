895cb1aa5d3d5809678fd971ecaf985f75768d16
Jon O'Reilly del Cerro <jonoreilly007@gmail.com>
2025-11-09 11:15:23 +0100

    final solucion escalar

--- SOLUCION_ESCALAR.s
+++ SOLUCION_ESCALAR.s
@@ -1,19 +1,21 @@
 .data
 
-        ; matriz NxN
-        MATRIZ_A: .double 11,12,13,21,22,23,31,33,33
-        
-        ; vector Nx1 
-        VECTOR_X: .double 1,2,3
-        
-        ; vector Nx1
-        RESULTADO_B: .space 24
-        
-        ; M = N = 3. P = 1
-        N: .word 3
-
         ; valor 0
         ZERO: .double 0
+
+        ; matriz NxN
+        MATRIZ_A: .double 11,12,13,14,15,16,21,22,23,24,25,26
+        MATRIZ_A_2: .double 31,32,33,34,35,36,41,42,43,44,45,46
+        MATRIZ_A_3: .double 51,52,53,54,55,56,61,62,63,64,65,66
+        
+        ; vector Nx1 
+        VECTOR_X: .double 1,2,3,4,5,6
+        
+        ; vector Nx1 (resultado esperado: 301,511,721,931,1141,1351)
+        RESULTADO_B: .space 48
+        
+        ; Size of matrix
+        N: .word 6
         
 .text
 
@@ -45,11 +47,14 @@
         
         ; r17: temp variable for jumps and such (temp)
 
-        ; Load n from N
-        lw r1, N                                        ; n = *N
+        ; r18,r19: summary calculation
+        ; f10-f30: summary
         
         ; Initialize Zero variable 
         ld f0, ZERO                                     ; zero = 0
+
+        ; Load n from N
+        lw r1, N                                        ; n = *N
 
         ; Set element size to 8 bytes (.double)
         addi r2, r0, 8                                  ; elementSize = 8
@@ -57,9 +62,8 @@
         setup_loop_i:
                 addi r3, r0, 0                          ; i = 0
         start_loop_i:
-                slt r17, r3, r1                         ; if i < n
-                beqz r17, end_loop_i                    ; then goto end_loop_i
-                nop
+                sge r17, r3, r1                         ; if i >= n
+                bnez r17, end_loop_i                    ; then goto end_loop_i
         body_loop_i:
 
                 ; Initialize Sum variable 
@@ -68,9 +72,8 @@
                 setup_loop_j:
                         addi r4, r0, 0                  ; j = 0
                 start_loop_j:
-                        slt r17, r4, r1                 ; if j < n
-                        beqz r17, end_loop_j            ; then goto end_loop_j
-                        nop
+                        sge r17, r4, r1                 ; if j >= n
+                        bnez r17, end_loop_j            ; then goto end_loop_j
                 body_loop_j:
 
                         ; Load elementA from MATRIZ_A[i][j]
@@ -92,20 +95,55 @@
                         addd f2, f2, f4                 ; sum = sum + mult
 
                 tail_loop_j:
-                        addi r3, r3, 1                  ; j++
+                        addi r4, r4, 1                  ; j++
                         j start_loop_j                  ; goto start_loop_j
-                        nop
                 end_loop_j:
                 
                 ; Store sum in RESULTADO_B[i]
                 mult r15, r3, r2                        ; offsetB = i * elementSize
                 addi r16, r15, RESULTADO_B              ; addressElementB = offsetB + addressB
-                sd 0(r14), f2                           ; *addressElementB = sum
+                sd 0(r16), f2                           ; *addressElementB = sum
                 
         tail_loop_i:
                 addi r3, r3, 1                          ; i++
                 j start_loop_i
-                nop
         end_loop_i:
+
+        start_summary:
+                addi r18, r0, RESULTADO_B               ; r18 = addressB
+                slti r19, r1, 1                         ; if n < 1
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f10, 0(r18)
+                slti r19, r1, 2                         ; if n < 2
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f12, 8(r18)
+                slti r19, r1, 3                         ; if n < 3
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f14, 16(r18)
+                slti r19, r1, 4                         ; if n < 4
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f16, 24(r18)
+                slti r19, r1, 5                         ; if n < 5
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f18, 32(r18)
+                slti r19, r1, 6                         ; if n < 6
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f20, 40(r18)
+                slti r19, r1, 7                         ; if n < 7
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f22, 48(r18)
+                slti r19, r1, 8                         ; if n < 8
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f24, 56(r18)
+                slti r19, r1, 9                         ; if n < 9
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f26, 64(r18)
+                slti r19, r1, 10                        ; if n < 10
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f28, 72(r18)
+                slti r19, r1, 11                        ; if n < 11
+                bnez r19, end_summary                   ; then goto end_summary
+                ld f30, 80(r18)
+        end_summary:        
 
         trap 6
